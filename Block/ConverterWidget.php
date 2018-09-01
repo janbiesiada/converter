f9784841e7b25a9d93b9ae2aaf48dd6af5c4182b
Jan Biesiada <jan.biesiada.88@gmail.com>
2018-09-01 21:16:23 +0200

    refactor

---
+++ Block/ConverterWidget.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace Jbdev\CurrencyConverter\Block;
+
+
+use Magento\Framework\View\Element\Template;
+use Magento\Widget\Block\BlockInterface;
+
+class ConverterWidget extends Template implements BlockInterface
+{
+
+}
